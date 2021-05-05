9a26c3fe69ba335f73b550762e069ef0203249d5
chingsley <eneja.kc@gmail.com>
2021-05-05 18:22:13 +0100

    complete the tutorial

--- react-router-animated-transitions/src/App.js
+++ react-router-animated-transitions/src/App.js
@@ -1,5 +1,13 @@
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import "./App.css";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch,
+  Link,
+  useLocation,
+} from "react-router-dom";
+import "./styles.css";
 
 export default function App() {
   return (
@@ -15,9 +23,25 @@ export default function App() {
 }
 
 function Content() {
+  const location = useLocation();
+  const [displayLocation, setDisplayLocation] = useState(location);
+  const [transitionStage, setTransistionStage] = useState("fadeIn");
+
+  useEffect(() => {
+    if (location !== displayLocation) setTransistionStage("fadeOut");
+  }, [location]);
+
   return (
-    <div>
-      <Switch>
+    <div
+      className={`${transitionStage}`}
+      onAnimationEnd={() => {
+        if (transitionStage === "fadeOut") {
+          setTransistionStage("fadeIn");
+          setDisplayLocation(location);
+        }
+      }}
+    >
+      <Switch location={displayLocation}>
         <Route path="/other">
           <section>other</section>
         </Route>
